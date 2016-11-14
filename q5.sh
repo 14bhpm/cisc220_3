470ff6996a2fe9f48c5fe887cecf2b9040525851
Omar Toutounji <omartoutounji@outlook.com>
2016-11-14 13:58:54 -0500

    Added comments

--- q5.sh
+++ q5.sh
@@ -3,6 +3,7 @@
 #Developed in CISC 220
 # Write a script that looks for all executable files in the current directory and adds
 # “#Developed in cisc220” just after the “#!/bin/bash” line.
+# Asked prof Shadi if I could just insert Developed in cisc220 after the first line, he said okay.
 
 sed -i '1 a #Developed in CISC 220' *.sh
 
