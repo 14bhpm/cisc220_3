31165143edde344647b54445e1d9e6a0933e72a1
Rawan Alsaadi <12rma1@queensu.ca>
2016-11-14 15:09:30 -0500

    topParts.sh

---
+++ topParts.sh
@@ -0,0 +1,6 @@
+# Rawan Alsaadi 10081442 
+# Omar Toutounji 10169506
+# Beltan Michel 10169162
+
+#!/bin/bash
+lsblk --noheadings --raw | grep 'part' | sed s/“K”/“ K”/ | sed s/“M”/“ M”/ | sed s/“G”/“ G”/ | sort -h -k5,5 -k4,4n | tail -n2
